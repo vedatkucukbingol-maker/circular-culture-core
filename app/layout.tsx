be391a07c63d9e0d2489fb39be9bdcd89cb1ab23
vedatkucukbingol-maker <vedatkucukbingol@gmail.com>
2026-03-13 16:23:01 +0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,6 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
 import "./globals.css";
 import { LanguageProvider } from "../context/LanguageContext";
-
-const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Circular Culture",
@@ -16,8 +13,11 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>
+    <html lang="tr">
+      <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </head>
+      <body>
         <LanguageProvider>
           {children}
         </LanguageProvider>
