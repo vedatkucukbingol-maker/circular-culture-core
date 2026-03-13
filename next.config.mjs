73dcc951b95c4372dff48d47fd86731de004362f
vedatkucukbingol-maker <vedatkucukbingol@gmail.com>
2026-03-13 15:33:33 +0300

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  // Gereksiz 'eslint' veya 'typescript' bloklarını kaldırın, sadece bunları bırakın
   typescript: { ignoreBuildErrors: true },
   eslint: { ignoreDuringBuilds: true },
 };
