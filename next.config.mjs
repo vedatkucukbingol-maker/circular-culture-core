7e1557fc319889e2cae22b2bd270704a2cfec8f7
vedatkucukbingol-maker <vedatkucukbingol@gmail.com>
2026-03-13 16:04:27 +0300

    Delete next.config.mjs

--- next.config.mjs
+++
@@ -1,8 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  // Gereksiz 'eslint' veya 'typescript' bloklarını kaldırın, sadece bunları bırakın
-  typescript: { ignoreBuildErrors: true },
-  eslint: { ignoreDuringBuilds: true },
-};
-
-export default nextConfig;
