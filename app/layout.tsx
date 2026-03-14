635de0b0ab7830c346d7d91dc500f48cc1e634bc
vedatkucukbingol-maker <vedatkucukbingol@gmail.com>
2026-03-14 22:19:35 +0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,10 @@
 import "./globals.css";
 import type { Metadata } from "next";
-import Navbar from "@/components/Navbar";
+import Navbar from "../components/Navbar";
 
 export const metadata: Metadata = {
   title: "Circular Culture",
-  description: "Döngüsel Dönüşüm Platformu",
+  description: "Sustainability Platform",
 };
 
 export default function RootLayout({
@@ -14,7 +14,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="tr">
-      <body className="bg-black text-white antialiased">
+      <body className="bg-black antialiased">
         <Navbar />
         {children}
       </body>
