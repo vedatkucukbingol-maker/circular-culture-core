a5fb721412b1bc1bc4a43976c6ae2800714fddc5
vedatkucukbingol-maker <vedatkucukbingol@gmail.com>
2026-03-14 16:06:24 +0300

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,14 +2,16 @@ import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./lib/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
       colors: {
-        primary: "#10b981", // Yeşil tonu (Sürdürülebilirlik için)
+        background: "var(--background)",
+        foreground: "var(--foreground)",
+        primary: "#10b981",
       },
     },
   },
