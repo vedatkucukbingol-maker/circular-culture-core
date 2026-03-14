ccc26923c4ca0936e7b5efb5e33473c1557056b3
vedatkucukbingol-maker <vedatkucukbingol@gmail.com>
2026-03-14 22:37:38 +0300

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,19 +4,9 @@ const config: Config = {
   content: [
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./lib/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {
-      colors: {
-        emerald: {
-          400: '#34d399',
-          500: '#10b981',
-          600: '#059669',
-          700: '#047857',
-        }
-      },
-    },
+    extend: {},
   },
   plugins: [],
 };
