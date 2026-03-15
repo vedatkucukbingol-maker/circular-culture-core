f948ac3b01e2fa87f8da5c9dd3c194a3a98aa431
vedatkucukbingol-maker <vedatkucukbingol@gmail.com>
2026-03-15 12:25:55 +0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,3 @@
-import "./globals.css";
 import type { Metadata } from "next";
 
 export const metadata: Metadata = {
@@ -13,7 +12,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="tr">
-      <body style={{ backgroundColor: 'black', color: 'white', margin: 0, fontFamily: 'sans-serif' }}>
+      <body style={{ backgroundColor: 'black', color: 'white', margin: 0 }}>
         {children}
       </body>
     </html>
